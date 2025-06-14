a91294e5f8921c5c90d2524bdef6aa12a05132e8
Aayush Khanal <khanal.aayush.ak@gmail.com>
2025-06-14 20:36:23 +0545

    working on footer

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import img_1 from "../../assets/google.svg";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <section className="flex bg-transparent sm:m-8 ">
+        <div className=" max-w-screen mx-auto flex flex-col items-center gap-4 shadow-md p-4 justify-center rounded-sm">
+          <img src={img_1} alt="Google Quiz" className="w-14 h-14" />
+        <a 
+        href="https://phishingquiz.withgoogle.com/" 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="button py-2 px-2 sm:py-3 sm:px-6 "
+        title="Phishing Quiz by Google"
+        >
+          Take a Phishing Quiz by Google
+        </a>
+        </div>
+      </section>
+
+      <footer className="bg-transparent  justify-center items-center">
+        <div className="bg-mint-green text-center py-4">
+          <p className="text-lg font-semibold text-auzure">
+            <strong> © 2023 PhishVanGuard. All rights reserved.</strong>
+          </p>
+          <p className="text-sm text-russian-Violet p-4 ">
+            Made with ❤️ by the PhishVanGuard Team
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
-import logo from './assets/react.svg'; 
-import Home from './Components/Home/Home';
-
+import logo from "./assets/logo.svg";
+import Home from "./Components/Home/Home";
 
 const Layout = () => {
   const [initialLoading, setInitialLoading] = useState(true);
@@ -14,33 +13,36 @@ const Layout = () => {
     const timer = setTimeout(() => {
       setInitialLoading(false);
     }, 1000);
-
     return () => clearTimeout(timer);
   }, []);
 
   if (initialLoading) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen bg-mint-green">
         <div className="flex flex-col items-center">
-          <img src={logo} alt="Loading..." className="w-20 h-20 animate-spin" />
+          <img 
+          src={logo} 
+          alt="Loading..."
+           className="w-20 h-20 animate-spin" 
+           />
 
-          <h1 className="text-2xl font-bold mt-4">
-            <span>Phsih</span>
-            <span>Van</span>
-            <span>Guard</span>
-          </h1>
+          <h1 className="text-5xl font-bold mt-4 animate-">
+            <span className=" font-secondary text-auzure">Phsih</span>
+            <span className=" text-jordy-blue ">Van</span>
+            <span className=" text-cornflower-blue" >Guard</span>
+          </h1> 
         </div>
       </div>
     );
   }
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-white">
       <Header />
       <Home />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 };
 

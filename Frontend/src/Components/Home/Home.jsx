1fc94b33ece7c4e48a639339259d60c3e2be0c24
Aayush Khanal <khanal.aayush.ak@gmail.com>
2025-07-24 19:32:14 +0545

    color change 02

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -134,7 +134,7 @@ const Home = () => {
           )}
           <div className="flex flex-col items-center w-full max-w-md">
             {result.map((result, index) => (
-              <div className="bg-gray-500 shadow-md rounded-lg p-4 w-full max-w-md">
+              <div className="bg-Alice-Blue shadow-md rounded-lg p-4 w-full max-w-md">
                 <ResultCard
                   //done the prblem of is_phishing
                   key={ResultCard + index}
